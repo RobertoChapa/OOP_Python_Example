e04092be5dd6abfc022bd7718053cd7cab1cf77d
Roberto Chapa <bobby.chapa@gmail.com>
2021-07-18 19:56:42 -0400

    Update shape_areas_oop.py

--- shape_areas_oop.py
+++ shape_areas_oop.py
@@ -1,7 +1,7 @@
 import math
 
 class Shape:
-    def __init__(self):
+    def __init__(self, shape):
         self.area = 0
         self.radius = 0
         self.side = 0
@@ -9,38 +9,41 @@ class Shape:
         self.h = 0
         self.w = 0
         self.l = 0
+        self.shape = shape
 
     # circle
     def set_radius(self, radius):
         self.radius = radius
 
-    def get_circle_area(self):
-        self.area = math.pi * self.radius ** 2
-        return self.area
-
     # square
     def set_side(self, side):
         self.side = side
-
-    def get_square_area(self):
-        self.area = self.side ** 2
-        return self.area
 
     # rectangle
     def set_width_length(self, w, l):
         self.w = w
         self.l = l
 
-    def get_rectangle_area(self):
-        self.area = self.w * self.l
-        return self.area
-
     # triangle
     def set_height_base(self,b,h):
        self.b = b
        self.h = h
 
-    def get_triangle_area(self):
-        self.area = 1 / 2 * self.b * self.h
+    def get_area(self):
+        if self.shape == 'c': # circle
+            self.area = math.pi * self.radius ** 2
+
+        elif self.shape == 's': # square
+            self.area = self.side ** 2
+
+        elif self.shape == 'r': # rectangle
+            self.area = self.w * self.l
+
+        elif self.shape == 't': # triangle
+            self.area = 1 / 2 * self.b * self.h
+
+        else:
+            self.area = 0 # no shape
+
         return self.area
 
